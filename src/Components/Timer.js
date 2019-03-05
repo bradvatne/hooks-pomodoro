6ce9a652cd31ef7696895c3db1457c4bf3d3a816
notbrad <45443057+notbrad@users.noreply.github.com>
2019-03-05 11:06:06 -0800

    Need to fix timer logic

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,34 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useInterval } from "../util.js";
 
-export const Timer = ({ timer }) => {
-  const [state, setState] = useState({ timer });
-
-  const thisTimer = state.timer;
+const Timer = ({ timer }) => {
+  const [time, setTime] = useState(timer);
+  const [isRunning, setRunning] = useState(false)
 
   const countDown = () => {
-    let time = thisTimer.split(":");
-    if (time[1] === "00") {
-      time[1] = "59";
-      time[0]--;
-    } else time[1]--;
-    time = time.join(":");
-    setState({ timer: time });
+    let newTime = time.split(":");
+    if (newTime[1] === "00") {
+      newTime[1] = "59";
+      newTime[0]--;
+    } else newTime[1]--;
+    newTime = newTime.join(":");
+    setTime(newTime);
   };
 
-  const startCountDown = () => {
-    for (let i = 0; i < 100; i++) {
-      setInterval(countDown(thisTimer), 1000);
-    }
-  };
+  useInterval(countDown, isRunning ? 1000 : null);
+  
 
   return (
     <div className="container col">
-      <h1 className="display-4">{thisTimer}</h1>
+      <h1 className="display-4">{time}</h1>
       <div className="row">
         <button
           type="button"
           className="btn btn-outline-success col ml-4 mr-1"
-          onClick={() => startCountDown()}
+          onClick={() => setRunning(!isRunning)}
         >
           Start
         </button>
