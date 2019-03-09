8df030d97724ffd31156f769303fd49f88768a04
notbrad <45443057+notbrad@users.noreply.github.com>
2019-03-09 09:11:12 -0800

    Replacing hard-coded values

--- src/Components/Timers.js
+++ src/Components/Timers.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import Timer from "./Timer";
 
+const timerOne="25:00"
+const timerTwo="5:00"
+const timerThree="15:00"
+
 const Timers = () => {
   return (
     <Fragment>
-      <Timer timer="25:00" />
-      <Timer timer="5:00" />
-      <Timer timer="15:00" />
+      <Timer timer={timerOne} />
+      <Timer timer={timerTwo} />
+      <Timer timer={timerThree} />
     </Fragment>
   );
 };
